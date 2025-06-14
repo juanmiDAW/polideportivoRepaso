0e2e1a1e5bda41ac3bf39ce81a9af071535bebf1
DAW <juanmiguel.valle@iesdonana.org>
2025-06-14 13:34:31 +0200

    se arregla el color de boton reservado que faltaba cambiar un color

--- resources/views/livewire/pista.blade.php
+++ resources/views/livewire/pista.blade.php
@@ -38,7 +38,7 @@
                                     wire:click="reservar('{{ $fecha_hora }}')">Reservar
                                 </button>
                             </td>
-                        @elseif ($reserva->user_id == auth()->id())
+                        @elseif ($reserva->user_id == auth()->user()->id)
                             <td>
                                 <button
                                     class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
@@ -48,7 +48,7 @@
                         @else
                             <td>
                                 <button
-                                    class="focus:outline-none text-white bg-grey-700 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">Reservado
+                                    class="focus:outline-none text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">Reservado
                                 </button>
                             </td>
                         @endif
